d38fc9d2a180eb7e32881d341c1a597fa5c27710
BoogieWoogie91 <49623384+BoogieWoogie91@users.noreply.github.com>
2021-02-04 17:55:15 +0300

    pravki

--- php/send.php
+++ php/send.php
@@ -1,11 +1,11 @@
 <?php
-$to      = '70570508@mail.ru';
+$to      = '70570509@mail.ru';
 $subject = 'Обратный звонок';
     $message = '
                     Имя: '.$_POST['name'].'
                     Телефон: '.$_POST['phone'].'
              ';
-  $headers .= "From: Отправитель <70570508@mail.ru>\r\n"; //Наименование и почта отправителя
+  $headers .= "From: Отправитель <70570509@mail.ru>\r\n"; //Наименование и почта отправителя
 
 mail($to, $subject, $message, $headers);
 ?>
